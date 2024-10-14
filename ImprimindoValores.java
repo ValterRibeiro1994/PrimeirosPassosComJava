27db2b8caacb0c1d0b84c1c895a37d1e6764d194
ValterRibeiro1994 <valtertert@gmail.com>
2024-10-13 21:49:08 -0300

    Add files via upload

---
+++ ImprimindoValores.java
@@ -0,0 +1,12 @@
+public class ImprimindoValores {
+    public static void main(String[] args) throws Exception {
+        
+        //conhecendo a função println
+        System.out.println("Hello, World lol!");
+        System.out.println("Tenho o tal do java no vscode lol ");
+        System.out.println(5 + 5);
+        System.out.println('c');
+        System.out.println(5 < 8);
+
+    }
+}
---
+++ declarando_variaveis.java
@@ -0,0 +1,19 @@
+public class declarando_variaveis {
+
+    public static void main(String[] args) {
+        int um_numero = 5;
+        float um_ponto_flutuante = 3.14f; // possui 4 bytes
+        char uma_letra = 'c';
+        double um_double = 5.6235; // possui 8 bytes
+        boolean um_boleano = true; //diferente de python começa com minusculas
+        String um_texto = "Meu nome é valter estou aprendendo Java, isso é magnifico";
+
+        System.out.println(um_numero);
+        System.out.println(um_ponto_flutuante);
+        System.out.println(uma_letra);
+        System.out.println(um_double);
+        System.out.println(um_boleano);
+        System.out.println(um_texto);
+    }
+    
+}
